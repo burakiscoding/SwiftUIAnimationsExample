dfb1374ba4fc585b72651421c6ad493f64feedc2
burakiscoding <burak.kurtarir35@hotmail.com>
2024-08-15 19:51:15 +0300

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Burak Kurtarır on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
